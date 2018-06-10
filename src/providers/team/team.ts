2793008cd32b106b461656a80652ed91b21e04f5
Jorge Vergara <javebratt@gmail.com>
2018-06-10 14:21:03 -0500

    Adds the Team Provider

--- src/providers/team/team.ts
+++ src/providers/team/team.ts
@@ -1,17 +1,75 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the TeamProvider provider.
+import { AngularFireAuth } from 'angularfire2/auth';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject
+} from 'angularfire2/database';
+import firebase from 'firebase/app';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class TeamProvider {
-
-  constructor(public http: HttpClient) {
-    console.log('Hello TeamProvider Provider');
+  public userId: string;
+  constructor(afAuth: AngularFireAuth, public afDb: AngularFireDatabase) {
+    afAuth.authState.subscribe(user => {
+      this.userId = user.uid;
+    });
   }
 
+  getAdminStatus(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.afDb
+        .object(`/userProfile/${this.userId}/teamAdmin`)
+        .snapshotChanges()
+        .subscribe(action => {
+          resolve(action.payload.val());
+        });
+    });
+  }
+
+  getUserProfile(): AngularFireObject<any> {
+    return this.afDb.object(`/userProfile/${this.userId}/`);
+  }
+
+  getMemberProfile(memberId: string): AngularFireObject<any> {
+    return this.afDb.object(`/userProfile/${memberId}/`);
+  }
+
+  getTeamMemberList(teamId: string): AngularFireList<any> {
+    return this.afDb.list(`/teamProfile/${teamId}/teamMembers`);
+  }
+
+  getTeamProfile(teamId: string): AngularFireObject<any> {
+    return this.afDb.object(`/teamProfile/${teamId}`);
+  }
+
+  createTask(taskName: string, memberId: string, memberName: string,
+    memberEmail: string
+  ): Promise<any> {
+    const taskRef: firebase.database.ThenableReference = this.afDb
+      .list(`/taskListByTeam/${this.userId}/`)
+      .push({});
+
+    return taskRef.set({
+      taskName, memberId, memberName, memberEmail,
+      completed: false, id: taskRef.key
+    });
+  }
+
+  getTaskList(teamId: string): AngularFireList<any> {
+    return this.afDb.list(`/taskListByTeam/${teamId}/`);
+  }
+
+  getMemberTaskList(teamId: string, memberId: string): AngularFireList<any> {
+    return this.afDb.list(`/taskListByTeam/${teamId}`, ref =>
+      ref.orderByChild('memberId').equalTo(memberId)
+    );
+  }
+
+  completeTask(teamId: string, taskId: string): Promise<any> {
+    return this.afDb
+      .object(`/taskListByTeam/${teamId}/${taskId}/`)
+      .update({ completed: true });
+  }
 }
